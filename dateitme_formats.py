d278c26442135439be823c41f24449480d06f31b
Piers Turner <piers.turner@physics.ox.ac.uk>
2024-05-01 20:46:38 +0100

    first commit

---
+++ dateitme_formats.py
@@ -0,0 +1,3 @@
+    # DATE: Stores date values in the format YYYY-MM-DD.
+    # TIME: Stores time values in the format HH:MM:SS.
+    # DATETIME: Stores a combination of date and time in the format YYYY-MM-DD HH:MM:SS.
